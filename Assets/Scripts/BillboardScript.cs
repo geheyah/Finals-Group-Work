f3a3737da88e7472db8d3176e258cf0189669f4d
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2023-04-19 00:24:57 +0800

    Added BillboardScript.

--- Assets/Scripts/BillboardScript.cs
+++ Assets/Scripts/BillboardScript.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BillboardScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private BillboardType billboardType;
+
+    [Header("Lock Rotation")]
+    [SerializeField] private bool lockX;
+    [SerializeField] private bool lockY;
+    [SerializeField] private bool lockZ;
+
+    private Vector3 originalRotation;
+
+    public enum BillboardType { LookAtCamera, CameraForward };
+    z
+    private void Awake()
     {
-        
+        originalRotation = transform.rotation.eulerAngles;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Use Late update so everything should have finished moving.
+    void LateUpdate()
     {
-        
+        // There are two ways people billboard things.
+        switch (billboardType)
+        {
+            case BillboardType.LookAtCamera:
+                transform.LookAt(Camera.main.transform.position, Vector3.up);
+                break;
+            case BillboardType.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            default:
+                break;
+        }
+        // Modify the rotation in Euler space to lock certain dimensions.
+        Vector3 rotation = transform.rotation.eulerAngles;
+        if (lockX) { rotation.x = originalRotation.x; }
+        if (lockY) { rotation.y = originalRotation.y; }
+        if (lockZ) { rotation.z = originalRotation.z; }
+        transform.rotation = Quaternion.Euler(rotation);
     }
 }
