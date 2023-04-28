d47b74f7edd3a66f07644a24432f78b9d672d681
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2023-04-29 01:35:30 +0800

    Made Folia a little cheeky with the hidden leaves + Added leavesComplete variable to PlayerProperties

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -5,10 +5,18 @@ public class PlayerProperties : MonoBehaviour
     public static PlayerProperties instance;
 
     public int missingLeaves = 0;
+    public bool leavesComplete = false;
 
     void Awake()
     {
         instance = this;
     }
 
+    void Update()
+    {
+        if (missingLeaves >= 5)
+        {
+            leavesComplete = true;
+        }
+    }
 }
