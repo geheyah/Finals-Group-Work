cb3dfc832aeb97e89ed4e087895a06e40519c34d
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2023-05-01 23:14:05 +0800

    On ending scene, mouse is now visible after entering a portal.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,14 +4,36 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     public string nextSceneName;
+    public bool showMouseOnEnter = false;
+
+    private bool cursorWasLocked = false;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player entered portal");
+            if (showMouseOnEnter)
+            {
+                Cursor.visible = true; // Show mouse cursor
+                Cursor.lockState = CursorLockMode.None; // Unlock cursor to allow movement
+                cursorWasLocked = false; // Remember that cursor was not locked before
+            }
             SceneManager.LoadScene(nextSceneName);
         }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player exited portal");
+            if (showMouseOnEnter && !cursorWasLocked)
+            {
+                Cursor.visible = false; // Hide mouse cursor
+                Cursor.lockState = CursorLockMode.Locked; // Lock cursor to center of screen
+                cursorWasLocked = true; // Remember that cursor was locked before
+            }
+        }
+    }
 }
